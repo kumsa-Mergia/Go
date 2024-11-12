6127e4d148ef0a0d6e84a93089dbb4b6d53b929a
kumsa-Mergia <kumsamergia@gmail.com>
2024-11-12 10:06:14 +0300

    Day Two Hello World in GoLang

---
+++ 30-Day/Day-2/go.mod
@@ -0,0 +1,3 @@
+module Day-2
+
+go 1.23.3
---
+++ 30-Day/Day-2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// This is a single-line comment in Go
+func main() {
+	/*
+	   This is a multi-line comment in Go.
+	   It can span multiple lines.
+	   Useful for explaining more complex code or for temporarily disabling code blocks.
+	*/
+	fmt.Println("Hello World")
+}
+
+/*
+To Run The Program on Terminal
+		:=> go mod init Day-2
+ 		:=> go rin main.go
+*/
