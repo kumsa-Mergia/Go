d55c88398d2043b134b1afcdbaa665cc72ef655c
kumsa-Mergia <kumsamergia@gmail.com>
2024-11-16 11:11:33 +0300

    Handling Errors with panic and recover

---
+++ 30-Day/Day-5/HandlingErrors.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func safeDivision(a, b int) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+		}
+	}()
+
+	if b == 0 {
+		panic("division by zero")
+	}
+
+	fmt.Println("Result:", a/b)
+}
+
+func main() {
+	safeDivision(10, 2)
+	safeDivision(10, 0)
+	fmt.Println("Program continues...")
+}
