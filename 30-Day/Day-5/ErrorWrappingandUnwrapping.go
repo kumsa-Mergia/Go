71bf4b5f028b52e939c5ebee2310696646a86885
kumsa-Mergia <kumsamergia@gmail.com>
2024-11-16 11:17:54 +0300

    Error Wrapping and Unwrapping

---
+++ 30-Day/Day-5/ErrorWrappingandUnwrapping.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func wrappedErrorExample() error {
+	baseErr := errors.New("base error")
+	return fmt.Errorf("wrapped error: %w", baseErr) // %w wraps the error
+}
+
+func main() {
+	err := wrappedErrorExample()
+	fmt.Println("Error:", err)
+
+	// Unwrapping the error
+	unwrappedErr := errors.Unwrap(err)
+	fmt.Println("Unwrapped Error:", unwrappedErr)
+}
